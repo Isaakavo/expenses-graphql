7011e1e594929419d4a03af367cf9376fe07e80e
isaakavo <isaakhaas96@gmail.com>
2023-10-28 13:12:24 -0600

    feat: remove unnecesary log

--- src/auth/resolve-user.ts
+++ src/auth/resolve-user.ts
@@ -6,7 +6,6 @@ export const resolverUser = async (req: IncomingMessage) => {
   const token = req.headers['x-session-key'] || '';
   try {
     const decodedUser = await verifyJwt(token as string);
-    logger.info(`Decoded user ${decodedUser.userId}`);
     return decodedUser;
   } catch (error) {
     logger.error('Failed to resolve user', error);
