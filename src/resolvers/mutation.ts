68f3dce4d9457688881435684f325eae2a30e465
isaakavo <isaakhaas96@gmail.com>
2024-03-02 15:20:20 -0600

    add error handling logic for create expense mutation

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -101,6 +101,23 @@ const mutations: MutationResolvers = {
     const {
       user: { userId },
     } = context;
+    const conceptLengthMax = 100;
+
+    // TODO improve error logic
+    if (concept.length === 0) {
+      logger.error('Concept is empty');
+      throw new Error('Concept must not be empty');
+    }
+
+    if (total === 0 || total < 0) {
+      logger.error('Total bad input');
+      throw new Error('Total must not be negative or zero');
+    }
+
+    if (concept.length > conceptLengthMax) {
+      logger.error('concept error');
+      throw new Error(`Concept lenght must be lower than ${conceptLengthMax}`);
+    }
 
     const card =
       cardId &&
