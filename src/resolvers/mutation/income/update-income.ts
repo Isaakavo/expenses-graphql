852a76606ce3eb6be637246099b3dee346522151
isaakavo <isaakhaas96@gmail.com>
2025-05-31 22:24:38 -0700

    create update income in income service

--- src/resolvers/mutation/income/update-income.ts
+++ src/resolvers/mutation/income/update-income.ts
@@ -1,32 +1,15 @@
+import { IncomeService } from '../../../service/income-service.js';
 import { adaptSingleIncome } from '../../../adapters/income-adapter.js';
 import { MutationResolvers } from '../../../generated/graphql.js';
-import { Income } from '../../../models/income.js';
-import { Date as CustomDate } from '../../../scalars/date.js';
-import { updateElement } from '../../../utils/sequilize-utils.js';
 
 export const updateIncome: MutationResolvers['updateIncome'] = async (
   _,
   { input },
   context
 ) => {
-  const {
-    user: { userId },
-  } = context;
-  const { incomeId, total, paymentDate, comment } = input;
+  const incomeService = new IncomeService();
 
-  const updateParams = {
-    total,
-    comment: comment?.trim() ?? '',
-    paymentDate: CustomDate.parseValue(paymentDate),
-    updatedAt: CustomDate.parseValue(new Date().toISOString()),
-  };
+  const updatedIncome = await incomeService.updaIncome(input, context);  
 
-  const updatedIncome = await updateElement(
-    Income,
-    userId,
-    incomeId,
-    updateParams
-  );
-
-  return adaptSingleIncome(updatedIncome[0] as Income);
+  return adaptSingleIncome(updatedIncome[0]);
 };
--- src/service/income-service.ts
+++ src/service/income-service.ts
@@ -2,11 +2,13 @@ import { GraphQLError } from 'graphql';
 import {
   CreateIncomeInput,
   MutationDeleteIncomeByIdArgs,
+  UpdateIncomeInput,
 } from '../generated/graphql.js';
 import { Context } from '../index.js';
 import { logger } from '../logger.js';
 import { Income } from '../models/income.js';
 import { Date as CustomDate } from '../scalars/date.js';
+import { validateId } from '../utils/sequilize-utils.js';
 
 export class IncomeService {
   async createIncome(input: CreateIncomeInput, context: Context) {
@@ -45,5 +47,45 @@ export class IncomeService {
 
     logger.info(`Deleted ${isDeleted} Income`);
     return true;
+  }
+
+  async updaIncome(input: UpdateIncomeInput, context: Context) {
+    const { incomeId, total, comment, paymentDate } = input;
+    const {
+      user: { userId },
+    } = context;
+
+    await validateId(Income, userId, incomeId);
+
+    const updateParams = {
+      total,
+      comment: comment?.trim() ?? '',
+      paymentDate: CustomDate.parseValue(paymentDate),
+      updatedAt: CustomDate.parseValue(new Date().toISOString()),
+    };
+
+    const [affectedCount, updatedElement] = await Income.update(
+      {
+        ...updateParams,
+      },
+      {
+        where: { id: incomeId, userId },
+        returning: true,
+      }
+    );
+
+    if (updatedElement.length === 0) {
+      logger.info('Couldt update Income, id doesnt exists');
+      throw new GraphQLError('Income id not found', {
+        extensions: {
+          code: 'NOT_FOUND',
+          http: { status: 404 },
+        },
+      });
+    }
+
+    logger.info(`Updated Income, affectedCount ${affectedCount}`);
+
+    return updatedElement;
   }
 }
