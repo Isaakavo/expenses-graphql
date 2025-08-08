7f8079a5ea800b7c0c9c309e4a3943776efa1418
isaakavo <isaakhaas96@gmail.com>
2025-08-07 22:19:45 -0700

    feat: implement income creation with category allocations and update logic

--- src/repository/income-repository.ts
+++ src/repository/income-repository.ts
@@ -1,16 +1,19 @@
 import { logger } from '../logger.js';
-import { sequelize } from '../database/index.js';
 import { Income } from '../models/index.js';
 import { Period } from '../models/periods.js';
 import { PeriodRepository } from './period-repository.js';
-import { FindOptions, Op } from 'sequelize';
+import { FindOptions, Op, Sequelize } from 'sequelize';
+import { CategorySettings } from 'models/category-settings.js';
+import { IncomeCategoryAllocation } from 'models/income-category-allocation.js';
 
 export class IncomeRepository {
   private periodRepository: PeriodRepository;
   userId: string;
+  sequelize: Sequelize;
 
-  constructor(userId: string) {
+  constructor(userId: string, sequelize: Sequelize) {
     this.userId = userId;
+    this.sequelize = sequelize;
     this.periodRepository = new PeriodRepository(userId);
   }
 
@@ -46,7 +49,7 @@ export class IncomeRepository {
       where.paymentDate = {
         [Op.between]: [startDate, endDate],
       };
-    }    
+    }
 
     return Income.findAll({
       where,
@@ -62,7 +65,7 @@ export class IncomeRepository {
   }
 
   async createIncome(incomeData: Partial<Income>) {
-    return await sequelize.transaction(async (t) => {
+    return await this.sequelize.transaction(async (t) => {
       const period: Period = await this.periodRepository.createPeriod(
         incomeData.paymentDate,
         { transaction: t }
@@ -98,6 +101,48 @@ export class IncomeRepository {
           `Income already exists for user ${this.userId} on date ${incomeData.paymentDate}`
         );
       }
+
+      // TODO this should be its own respository
+      const categorySetting = await CategorySettings.findAll({
+        where: {
+          userId: this.userId,
+        },
+      });
+
+      await Promise.all(
+        categorySetting.map(async (setting) => {
+          const [allocation, created] =
+          // TODO this should be its own respository
+            await IncomeCategoryAllocation.findOrCreate({
+              where: {
+                userId: this.userId,
+                incomeId: income.id,
+                categoryId: setting.categoryId,
+              },
+              defaults: {
+                percentage: setting.percentage,
+                amountAllocated: income.total * setting.percentage,
+              },
+              transaction: t,
+            });
+
+          if (!created) {
+            // Update allocation if percentage or amountAllocated changed
+            if (
+              allocation.percentage !== setting.percentage ||
+              allocation.amountAllocated !== income.total * setting.percentage
+            ) {
+              await allocation.update(
+                {
+                  percentage: setting.percentage,
+                  amountAllocated: income.total * setting.percentage,
+                },
+                { transaction: t }
+              );
+            }
+          }
+        })
+      );
 
       const incomeWithPeriod = {
         ...income.get(),
