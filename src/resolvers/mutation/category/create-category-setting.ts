fe08b9ecac843dac8d88546772b19f1ac8acef5c
isaakavo <isaakhaas96@gmail.com>
2025-11-18 18:47:31 -0800

    fix: add logger for error on create-category-setting.ts

--- src/resolvers/mutation/category/create-category-setting.ts
+++ src/resolvers/mutation/category/create-category-setting.ts
@@ -1,5 +1,6 @@
 import { MutationResolvers } from 'generated/graphql';
 import { CategorySettingsService } from '../../../service/category-setting-service.js';
+import {logger} from '../../../logger.js';
 
 export const createCategorySetting: MutationResolvers['createCategorySetting'] =
   async (
@@ -24,6 +25,7 @@ export const createCategorySetting: MutationResolvers['createCategorySetting'] =
         message: 'Category setting created successfully',
       };
     } catch (e) {
+      logger.error(e.message)
       throw e
     }
   };
