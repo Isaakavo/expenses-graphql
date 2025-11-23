841a9284c9f4a77d600a58fac68a12ae1cb64d77
isaakavo <isaakhaas96@gmail.com>
2025-11-22 18:09:00 -0800

    chore: include env files to handle secrets

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const sequelizeClient =
   process.env.NODE_ENV === 'production'
@@ -7,10 +10,10 @@ export const sequelizeClient =
     })
     : new Sequelize({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'admin',
-      database: 'expenses',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       logging: false,
     });
